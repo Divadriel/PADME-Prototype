bef8dde49503b46488bb444ff4629393b9099c8d
David Rei <rei.david26@gmail.com>
2020-05-15 16:40:30 +0200

    new class to handle all aspects of simulation

---
+++ src/fr/limsi/Model/Programme.java
@@ -0,0 +1,15 @@
+package fr.limsi.Model;
+
+import java.util.ArrayList;
+
+public class Programme {
+
+    private ArrayList<Exercise> exerciseArrayList;
+    private ArrayList<Session> sessionArrayList;
+    private UserModel user;
+    private AdaptationRules adaptationRules;
+
+    public Programme(){
+
+    }
+}
