ee16e650f7810dbc07e35a8c25f046e7d27f1c88
David Rei <rei.david26@gmail.com>
2020-06-19 11:20:40 +0200

    added neutral color hues (black / grey / white)

--- src/fr/limsi/Model/Utils/Colors.java
+++ src/fr/limsi/Model/Utils/Colors.java
@@ -17,6 +17,10 @@ public class Colors {
     public static final Color PROM_COL_DARKER = Color.rgb(147,74,0);
     public static final Color PROM_COL_DARKEST = Color.rgb(107,54,0);
 
+    public static final Color NEUT_COL_MAIN = Color.rgb(51,51,51);
+    public static final Color NEUT_COL_BRIGHTER = Color.rgb(133,133,133);
+    public static final Color NEUT_COL_DARK = Color.rgb(31,31,31);
+    public static final Color NEUT_COL_DARKER = Color.rgb(20,20,20);
     public static final Color NEUTRAL_COL_DARKEST = Color.rgb(0,0,0);
     public static final Color NEUTRAL_COL_BRIGHTEST = Color.rgb(255,255,255);
 
